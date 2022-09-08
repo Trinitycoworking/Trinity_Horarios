1d677942eb9d0d4a836493045253594b56d410f4
André Fernandes <estagiario.trinity@hotmail.com>
2022-09-08 16:08:16 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <iframe scrolling="no" src="https://onedrive.live.com/embed?resid=1936239876C2833B%21285&authkey=%21AErG0UbGpEsvB3k&em=2&wdAllowInteractivity=False&AllowTyping=True&ActiveCell='Consultar%20hor%C3%A1rios%20(2)'!B2&Item='Consultar%20hor%C3%A1rios%20(2)'!B2%3AF66&wdHideGridlines=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=false"></iframe>
+      <iframe scrolling="no" src="https://onedrive.live.com/embed?resid=1936239876C2833B%21285&authkey=%21AErG0UbGpEsvB3k&em=2&wdAllowInteractivity=False&AllowTyping=True&ActiveCell='Consultar'!C3&Item='Consultar'!B2%3AF66&wdHideGridlines=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=false"></iframe>
     </div>
   )
 }
