dba5d5b68be4abe57d790e386812d21063b4792b
André Fernandes <estagiario.trinity@hotmail.com>
2022-09-08 15:30:19 -0300

    deploy

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <iframe scrolling="no" src="https://onedrive.live.com/embed?resid=1936239876C2833B%21285&authkey=%21AErG0UbGpEsvB3k&em=2&wdAllowInteractivity=False&AllowTyping=True&ActiveCell='Consultar%20hor%C3%A1rios%20(2)'!B2&Item='Consultar%20hor%C3%A1rios%20(2)'!B2%3AF66&wdHideGridlines=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=false"></iframe>
+    </div>
+  )
+}
+
+export default App
