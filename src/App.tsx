411283ed38624aa59dc986647a90fd6399986bb6
trinitycoworking <estagiario.trinity@hotmail.com>
2023-05-12 15:13:29 -0300

    atualização da planilha e layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <iframe scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%211625&authkey=%21AEkKhplncQiKQQg&em=2&wdAllowInteractivity=False&AllowTyping=True&ActiveCell='Consultar'!C3&Item='Consultar'!B1%3AF68&wdHideGridlines=True&wdDownloadButton=False&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=false"></iframe>
+      <div className='container'>
+      <iframe width="510" height="1610" frameBorder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%211625&authkey=%21AEkKhplncQiKQQg&em=2&wdAllowInteractivity=False&Item='Consultar'!A1%3AF77&wdHideGridlines=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=true&ed1JS=false"></iframe>
+      </div>
     </div>
   )
 }
