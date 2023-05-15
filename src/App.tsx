b17caceef4cb894c2cd84a25f0d913cc213ff829
trinitycoworking <estagiario.trinity@hotmail.com>
2023-05-15 13:27:33 -0300

    atualização da planilha

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-      <iframe width="510" height="1610" frameBorder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%211625&authkey=%21AEkKhplncQiKQQg&em=2&wdAllowInteractivity=False&Item='Consultar'!A1%3AF77&wdHideGridlines=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=true&ed1JS=false"></iframe>
+      <iframe width="510" height="1610" frameBorder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%211625&authkey=%21AEkKhplncQiKQQg&em=2&wdAllowInteractivity=False&AllowTyping=True&Item='Consultar'!A1%3AF77&wdHideGridlines=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=true&ed1JS=True"></iframe>
       </div>
     </div>
   )
