83e7211cc5967632f85b919f045ff4b75caf3ed4
Trinitycoworking <113135796+Trinitycoworking@users.noreply.github.com>
2023-08-16 18:34:16 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-      <iframe width="510" height="1610" frameBorder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%211625&authkey=%21AEkKhplncQiKQQg&em=2&wdAllowInteractivity=False&AllowTyping=True&Item='Consultar'!A1%3AF77&wdHideGridlines=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&resen=true&ed1JS=True"></iframe>
+      <iframe width="595" height="900" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=BE09E1A7AD9639C1%212533&authkey=%21AFe-AEd3wZmXjw8&em=2&wdAllowInteractivity=False&AllowTyping=True&ActiveCell='Consultar'!C3&Item='Consultar'!A1%3AG78&wdHideGridlines=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True"></iframe>
       </div>
     </div>
   )
